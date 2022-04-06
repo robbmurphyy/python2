160923a9db1dbdf058a1e0672e3a0024c26d994f
robbmurphyy <rmurphy272@g.rwu.edu>
2022-04-06 15:03:06 -0400

    assignment 6 final version

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 #Author: Robert Murphy
 #Class: Python 307
-#Midterm - main file
-#date 3/18/22
+#Assignment 6 webscraping
+#date 4/7/22
 
-import psutil
-import find_currrent_network_Midterm
+from selenium import webdriver
 
-if __name__ == '__main__':
-    print("Run this on WINDOWS!!!!!!\n")
-    if psutil.WINDOWS != True:
-        print("This program can only be run on WINDOWS!\n")
+#returns a new url that is acceptable by selenium
+def create_url(site_to_visit):
+    domain = "https://" + site_to_visit
+    print("Opening: ", domain)
+    return domain
+
+#Asks the user for a site to visit
+def ask_for_domain():
+    site_to_visit = input("Enter a URL to visit in this format: www.<domain>.com\n")
+    if "www." in site_to_visit and ".com" in site_to_visit: #check to see if www. and .com were entered
+        new_url = create_url(site_to_visit)
+        return new_url
     else:
-        ip = find_currrent_network_Midterm.discover_current_ip_and_network()
-        print("\nHere are the WI-FI connected devices:")
-        find_currrent_network_Midterm.scan_for_connected_hosts(ip)
+        print("improper formatting of URL")
+        return 1
 
+#uses selenium to go to user's url
+def visit_site(url):
+    firefox = webdriver.Firefox()
+    firefox.get(url)
 
-
-
-
+if __name__ == "__main__":
+    print("Requires geckodriver.exe to be in PATH")
+    url = ask_for_domain()
+    if url == 1:
+        print("exiting")
+    else:
+        visit_site(url)
